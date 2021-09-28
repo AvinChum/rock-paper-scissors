70597345f038ca520c5227fa3634aa9c7155fa50
Avi Chum <avinbenshemesh92@gmail.com>
2021-09-27 21:45:08 -0400

    deleted userPick function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function computerPlay () {
+function computerPlay() {
     let computerPick = Math.floor(Math.random()* 3) + 1;
 
     switch(computerPick) {
@@ -19,19 +19,18 @@ function computerPlay () {
 }
 
 
-function userPlay () {
-    let userPlay1 = prompt("Please pick your choice. Rock, Paper or Scissors").charAt(0).toLowerCase()
-    return userPlay1
-}
-
 
 function playRound (playerSelection, computerSelection) {
-     let playerpick1 = playerSelection
-     playerSelection.charAt(0).toUpperCase()
+
+
+    let playerpick1 = playerSelection
+    playerSelection.charAt(0).toUpperCase()
+
+
     if (playerpick1 === computerSelection) {
         return "It's a Tie." 
     }
-    else if (playerpick1=== "rock" && computerSelection === "scissors" ) {
+    else if (playerpick1 === "rock" && computerSelection === "scissors" ) {
         return "You win, Rock beats Scissors!"
     }
     else if (playerpick1 === "scissors" && computerSelection === "paper") {
@@ -49,4 +48,7 @@ function playRound (playerSelection, computerSelection) {
         return "You lose, Paper beats Rock"
     }
 }
-console.log(playRound("rock", "paper"))
+
+function game () {
+    
+}
